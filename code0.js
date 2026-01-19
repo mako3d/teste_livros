6b1da4852530ed1bb3bccf5b1d366b0cd4009f90
mako3d <63620416+mako3d@users.noreply.github.com>
2026-01-19 19:55:34 -0300

    Add files via upload

--- code0.js
+++ code0.js
@@ -9,10 +9,18 @@ gdjs.Untitled_32sceneCode.eventsList0 = function(runtimeScene) {
 
 {
 
+gdjs.copyArray(runtimeScene.getObjects("BlackDecoratedButton"), gdjs.Untitled_32sceneCode.GDBlackDecoratedButtonObjects1);
 
 let isConditionTrue_0 = false;
 isConditionTrue_0 = false;
-isConditionTrue_0 = gdjs.evtTools.input.isMouseButtonPressed(runtimeScene, "Left");
+for (var i = 0, k = 0, l = gdjs.Untitled_32sceneCode.GDBlackDecoratedButtonObjects1.length;i<l;++i) {
+    if ( gdjs.Untitled_32sceneCode.GDBlackDecoratedButtonObjects1[i].IsClicked(null) ) {
+        isConditionTrue_0 = true;
+        gdjs.Untitled_32sceneCode.GDBlackDecoratedButtonObjects1[k] = gdjs.Untitled_32sceneCode.GDBlackDecoratedButtonObjects1[i];
+        ++k;
+    }
+}
+gdjs.Untitled_32sceneCode.GDBlackDecoratedButtonObjects1.length = k;
 if (isConditionTrue_0) {
 {gdjs.evtTools.window.setFullScreen(runtimeScene, true, true);
 }
